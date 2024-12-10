17e93c06838a22854ec536ebad85f3e1dc2e72e8
Janak Koradiya <63702103+janakkoradiya@users.noreply.github.com>
2024-12-10 22:20:46 +0530

    remove style

--- pages/auth/reset-password.js
+++ pages/auth/reset-password.js
@@ -4,8 +4,8 @@ const ResetPassword = () => {
     const router = useRouter();
 
     return (
-        <div className={styles.container}>
-            <h1 className={styles.heading}>Welcome to the ResetPassword Page</h1>
+        <div >
+            <h1 >Welcome to the ResetPassword Page</h1>
             <button onClick={() => router.push('./contact')}>Contact</button>
             <button onClick={() => router.push('./about')}>About</button>
         </div>
